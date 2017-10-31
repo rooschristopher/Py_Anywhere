23993bd317072313e1916be60cdafab0b74454ec
Christopher Roos <Christopher.Roos@Stukent.com>
2017-10-31 11:16:10 -0600

    Added Home_Page

--- Py_Anywhere.py
+++ Py_Anywhere.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return flask.render_template('Home_Page.html')
+
 
 
 if __name__ == '__main__':
