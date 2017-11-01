a3b2c3d1c36ff60bb60e658153c917a0a9f0667e
Christopher Roos <Christopher.Roos@Stukent.com>
2017-11-01 16:29:47 -0600

    Added Testing for comments

--- Py_Anywhere.py
+++ Py_Anywhere.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="<christopherroos>",
-    password="<r00ster>",
-    hostname="<christopherroos.mysql.pythonanywhere-services.com>",
-    databasename="<christopherroos$Test>",
+    username="christopherroos",
+    password="r00sterr00ster",
+    hostname="christopherroos.mysql.pythonanywhere-services.com",
+    databasename="christopherroos$Test",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
@@ -21,6 +21,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
+comments = ['THis is the first one ', 'second one', 'third one']
+
+
+
+
+
 class Comment(db.Model):
 
     __tablename__ = "comments"
@@ -28,12 +35,15 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-
+for i in comments:
+    comment = Comment(content=i)
+    db.session.add(comment)
+    db.session.commit()
 
 
 @app.route('/')
 def hello_world():
-    return flask.render_template('Home_Page.html')
+    return flask.render_template('Home_Page.html' , comments=Comment.query.all())
 
 
 
